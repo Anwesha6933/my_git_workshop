fdbfe46e4c77cd9c179b60c4f54a2179df4578b9
Anwesha <dashanwesha07@gmail.com>
2026-01-21 12:24:14 +0530

    Updated main.py to include multiply and divide modules

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from sum import add
 from difference import subtract
+from multiply import multiply
+from divide import divide
 
-print("Sum:", add(5, 3))
-print("Difference:", subtract(5, 3))
+print("Sum:", add(10, 5))
+print("Difference:", subtract(10, 5))
+print("Product:", multiply(10, 5))
+print("Quotient:", divide(10, 5))
